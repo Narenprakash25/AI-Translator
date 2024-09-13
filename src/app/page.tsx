38c26592f96a1b8ead273f9010c80d2deb797edd
narendracarboncompete <narendra@carboncompete.com>
2024-09-13 23:41:15 +0530

    TextArea error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import TextArea from '@/app/components/Inputs/TextArea';
+import React, { useState, ChangeEvent } from "react";
+
 
 export default function Home() {
+  const [sourceText, setSourceText]= useState<string>("");
   return (
     <div>
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
@@ -9,11 +13,33 @@ export default function Home() {
      <div className="relative overflow-hidden h-screen">
       <div className="max-w-[85rem] mx-auto px-4 sm:px-6 py-10 sm:py-24">
         <div className="text-center">
-          <h1 className="text-4xl sm:text-6xl">
-            Narendra Prakash
+          <h1 className="text-4xl sm:text-6xl font-bold">
+            Language <span className="text-[#f87315]">Speak</span>
           </h1>
-        </div>
+          <p className="mt-3 text-neutral-400 font-bold">
+            LinguaSpeak: Bridging Voices ,Connecting Worlds
+          </p>
 
+          <div className="mt-7 sm:mt-12 mx-auto max-w--3xl relative">
+            <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
+              <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20"> 
+                <TextArea
+                id='source-language'
+                placeholder={'Source Language'}
+                value={sourceText}
+                onChange={(e:ChangeEvent<HTMLTextAreaElement>)=>{
+                  setSourceText(e.target.value)
+                }}
+                 
+                >
+
+      
+              </div>
+
+            </div>
+
+          </div>
+        </div>
       </div>
 
      </div>
